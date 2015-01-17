d9f2b61a50702a44c8a2fe15f49adeebe96e3053
Robert van der Linde <r.vanderlinde@pharma-intelligence.nl>
2015-01-17 22:42:13 +0100

    The server itself
    Signed-off-by: Robert van der Linde <r.vanderlinde@pharma-intelligence.nl>

---
+++ src/Server.php
@@ -0,0 +1,51 @@
+<?php
+namespace PharmaIntelligence\MLLP;
+
+use Evenement\EventEmitter;
+use Evenement\EventEmitterInterface;
+use React\Socket\ConnectionInterface;
+
+
+abstract class Server extends EventEmitter implements EventEmitterInterface
+{
+    private $io;
+    
+    public function __construct(EventEmitterInterface $io) {
+        $this->io = $io;
+        $this->io->on('connection', function(ConnectionInterface $connection) {
+            $this->handleRequest($connection);
+        });
+    }
+    
+    public function handleRequest(ConnectionInterface $connection) {
+        $this->emit('connection', array($connection));
+        $connection->on('data', function($data) use ($connection) {
+            try {
+                $data = MLLPParser::unwrap($data);
+                $this->emit('data', array($data, $connection));
+            } catch(\InvalidArgumentException $e) {
+                $this->handleInvalidMLLPEnvelope($data, $connection);
+                $this->emit('error', array('Invalid MLLP envelope. Received: "'.$data.'"'));
+            }
+        });
+    }
+    
+    public function send($data, ConnectionInterface $connection) {
+        $this->emit('send', array($data));
+        
+        $connection->on('error', function(ConnectionInterface $connection, $error) {
+           $this->emit('error', array('Error sending data: '.$error));
+        });
+        
+        $data = MLLPParser::enclose($data);
+        $connection->write($data);
+        $connection->removeAllListeners('error');
+        
+    }
+    
+    protected function handleInvalidMLLPEnvelope($data, ConnectionInterface $connection) {
+        $connection->end('INVALID ENVELOPE');
+    }
+}
+
+?>
